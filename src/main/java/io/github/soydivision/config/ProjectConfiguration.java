127ea9490c8d6fd63c36678ce0467021e122abd4
Sadvakassov Bagzhan <sadvakasov.bagjan@gmail.com>
2023-06-15 12:00:01 +0600

    primary anno omitted, conf using name++

--- src/main/java/io/github/soydivision/config/ProjectConfiguration.java
+++ src/main/java/io/github/soydivision/config/ProjectConfiguration.java
@@ -3,6 +3,7 @@ package io.github.soydivision.config;
 import io.github.soydivision.beans.BeanExample;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class ProjectConfiguration {
@@ -10,8 +11,14 @@ public class ProjectConfiguration {
     @Bean
     public BeanExample beanExample() {
         BeanExample beanExample = new BeanExample();
-        beanExample.setText("sexy");
+        beanExample.setText("text from bean 1");
         return beanExample;
     }
 
+    @Bean
+    public BeanExample beanExample2() {
+        BeanExample beanExample = new BeanExample();
+        beanExample.setText("text from bean 2");
+        return beanExample;
+    }
 }
--- src/main/java/io/github/soydivision/main/Main.java
+++ src/main/java/io/github/soydivision/main/Main.java
@@ -7,8 +7,12 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Main {
     public static void main(String[] args) {
         try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfiguration.class)) {
-            BeanExample beanExample = context.getBean(BeanExample.class);
-            System.out.println(beanExample.getText());
+            BeanExample beanExample1 = context.getBean("beanExample", BeanExample.class);
+            BeanExample beanExample2 = context.getBean("beanExample2", BeanExample.class);
+            BeanExample beanExample3 = context.getBean("beanExample", BeanExample.class);
+            System.out.println(beanExample1.getText());
+            System.out.println(beanExample2.getText());
+            System.out.println(beanExample3.getText());
         }
     }
 }
