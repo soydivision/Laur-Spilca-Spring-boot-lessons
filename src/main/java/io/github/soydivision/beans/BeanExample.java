2acbcfbc864f42d8a624818ca165cc0b5bb12160
Sadvakassov Bagzhan <sadvakasov.bagjan@gmail.com>
2023-06-15 12:48:35 +0600

    post construct ann. is used to set the text in beans

--- src/main/java/io/github/soydivision/beans/BeanExample.java
+++ src/main/java/io/github/soydivision/beans/BeanExample.java
@@ -1,11 +1,17 @@
 package io.github.soydivision.beans;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BeanExample {
     private String text;
 
+
+    @PostConstruct
+    private void postConstructInitializer() {
+        this.text = "PC text";
+    }
 
     public String getText() {
         return text;
