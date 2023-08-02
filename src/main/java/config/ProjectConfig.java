0a8f0ced2e55423bdaa181fa56d0087e873983f6
Sadvakassov Bagzhan <sadvakasov.bagjan@gmail.com>
2023-08-02 11:31:13 +0600

    beans called using names now

--- src/main/java/config/ProjectConfig.java
+++ src/main/java/config/ProjectConfig.java
@@ -9,22 +9,22 @@ import org.springframework.context.annotation.Primary;
 @Configuration
 public class ProjectConfig {
 
-    @Bean
+    @Bean("A")
     public MyBean myBean() {
         MyBean myBean = new MyBean();
-        myBean.setText("some text");
+        myBean.setText("Text from bean 0");
         return myBean;
     }
 
     @Primary
-    @Bean
+    @Bean("B")
     public MyBean myBean1() {
         MyBean b = new MyBean();
         b.setText("Text from bean 1");
         return b;
     }
 
-    @Bean
+    @Bean("C")
     public MyBean myBean2() {
         MyBean b = new MyBean();
         b.setText("Text from bean 2");
