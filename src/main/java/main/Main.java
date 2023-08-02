5050debd9492d6e24dbdb4705b5828d16dc48af8
Sadvakassov Bagzhan <sadvakasov.bagjan@gmail.com>
2023-08-02 11:25:30 +0600

    bean called using name

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -8,8 +8,8 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Main {
     public static void main(String[] args) {
         try (var context = new AnnotationConfigApplicationContext(ProjectConfig.class)) {
-            MyBean b1 = context.getBean(MyBean.class);
-            MyBean b2 = context.getBean(MyBean.class);
+            MyBean b1 = context.getBean("myBean2", MyBean.class);
+            MyBean b2 = context.getBean("myBean2",MyBean.class);
             MyBean b3 = context.getBean(MyBean.class);
             System.out.println(b1.getText());
             System.out.println(b2.getText());
