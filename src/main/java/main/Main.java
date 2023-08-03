3b9df7c9d1f91594a1e78589f6ee65bd64d55157
Sadvakassov Bagzhan <sadvakasov.bagjan@gmail.com>
2023-08-03 12:09:06 +0600

    remove blank line

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -8,7 +8,6 @@ import services.ProductDeliveryService;
 public class Main {
     public static void main(String[] args) {
         try (var context = new AnnotationConfigApplicationContext(ProjectConfig.class)) {
-
             ProductDeliveryService productDeliveryService = context.getBean(ProductDeliveryService.class);
             productDeliveryService.addProduct();
         }
