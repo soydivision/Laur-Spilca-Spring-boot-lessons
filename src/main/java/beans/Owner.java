30baa963304ffbf8c6ed88d1b428d9e8d429a531
Sadvakassov Bagzhan <sadvakasov.bagjan@gmail.com>
2023-08-10 11:43:41 +0600

    cat autowired thru setter now

--- src/main/java/beans/Owner.java
+++ src/main/java/beans/Owner.java
@@ -5,20 +5,20 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Owner {
-    private final Cat cat;
+    private  Cat cat;
 
-    @Autowired
-    public Owner(Cat cat) {
-        this.cat = cat;
-    }
+//    public Owner(Cat cat) {
+//        this.cat = cat;
+//    }
 
     public Cat getCat() {
         return cat;
     }
 
-//    public void setCat(Cat cat) {
-//        this.cat = cat;
-//    }
+    @Autowired
+    public void setCat(Cat cat) {
+        this.cat = cat;
+    }
 
     @Override
     public String toString() {
