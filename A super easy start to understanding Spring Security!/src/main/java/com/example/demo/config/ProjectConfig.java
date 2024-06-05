c7d4503d0e3cc48f6f465fde5e3494d19d46e2a2
Bagzhan Sadvakassov <sadvakasov.bagjan@gmail.com>
2024-06-05 12:23:47 +0500

    ref

--- A super easy start to understanding Spring Security!/src/main/java/com/example/demo/config/ProjectConfig.java
+++ A super easy start to understanding Spring Security!/src/main/java/com/example/demo/config/ProjectConfig.java
@@ -1,8 +1,6 @@
 package com.example.demo.config;
 
-import com.example.demo.security.AppUserDetailsManager;
 import com.example.demo.security.PlainTextPasswordEncoder;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
